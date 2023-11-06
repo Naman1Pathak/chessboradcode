b7fa2fdf28a90f5ff346dc4f5d35778e6101d925
Naman Pathak <pathaknaman638@gmail.com>
2023-11-06 22:21:55 +0530

    firstcommit

---
+++ naman/javaBasics.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class javaBasics
+{
+	public static void main(String args[]) {
+    //Scanner sc = new Scanner(System.in);
+    
+    // String input =sc.next();
+    // System.out.println(input);
+    // String name=sc.nextLine();
+    // System.out.println(name);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // int product= a*b;
+    // System.out.println(product);
+      // float rad= sc.nextFloat();
+      // float area = 3.14f*rad*rad;
+      // System.out.println(area);
+      // float a = 25.12f;
+      // int b =(int) a;
+      char a = 'a';
+      char b = 'b';
+
+      System.out.println(b-a);
+
+    } 
+}
